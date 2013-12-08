55e157d5e6dd0697e05c2d4bd74e9681d123873e
WeAthFolD <619756567@qq.com>
2013-12-08 13:37:24 +0800

    Tuotrial-01-commit

---
+++ graphrevo/proxy/GRClientProxy.java
@@ -0,0 +1,24 @@
+/**
+ * Code by Lambda Innovation, 2013.
+ */
+package cn.graphrevo.proxy;
+
+/**
+ * 客户端的加载代理。
+ * @author WeAthFolD
+ */
+public class GRClientProxy extends GRCommonProxy {
+
+	public void preInit() {
+		super.preInit();
+	}
+	
+	public void init() {
+		super.init();
+	}
+	
+	public void postInit() {
+		super.postInit();
+	}
+
+}
